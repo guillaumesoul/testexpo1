277cbf7badd8ba997e6976106c10996df260ff46
guillaumesoul <guillaume.soullard@gmail.com>
2014-03-17 11:57:02 +0100

    display liste slideshows

---
+++ client/slideshows/slideshows_list.js
@@ -0,0 +1,17 @@
+var postsData = [ {
+    title: 'Slideshow1',
+    author: 'Sacha Greif',
+    url: 'http://sachagreif.com/introducing-telescope/'
+}, {
+    title: 'Slideshow2',
+    author: 'Tom Coleman',
+    url: 'http://meteor.com'
+}, {
+    title: 'Slideshow3',
+    author: 'Tom Coleman',
+    url: 'http://themeteorbook.com'
+} ];
+
+
+Template.slideshowsList.helpers({ slideshows: postsData
+});
